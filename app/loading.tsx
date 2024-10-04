ea3a0eb7d3142815c8dedd1163ccf871e20d270d
CodewithMK180105 <manishkumar180105@gmail.com>
2024-10-04 19:37:45 +0530

    Loader added

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+/* From Uiverse.io by G4b413l */
+
+export default function Loader() {
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+      <div className="newtons-cradle">
+        <div className="newtons-cradle__dot"></div>
+        <div className="newtons-cradle__dot"></div>
+        <div className="newtons-cradle__dot"></div>
+        <div className="newtons-cradle__dot"></div>
+      </div>
+    </div>
+  );
+}
